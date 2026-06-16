1545147c18906e8b26c077fc727e4e1fc7aecca9
shifucun1 <guchixsh@163.com>
2026-06-16 19:02:44 +0800

    support cloudflare and github pages builds

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/shifucun-portfolio/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/shifucun-portfolio/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 });
